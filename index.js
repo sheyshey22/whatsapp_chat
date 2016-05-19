239343ae461b209234f87f0830448a2dc1db632e
sheyla <s.vilca.neira@gmail.com>
2016-05-19 11:53:38 -0500

    ya funciona

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express= require('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -7,9 +7,10 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+
 io.on('connection', function(socket){
-	console.log('Tenemos una nueva vìctima!!');
-	
+	console.log('Tenemos una nueva victima!!');
+
 	socket.on('chat', function(_msg){
 		io.emit('nuevo_mensaje', _msg);
 	});
@@ -17,6 +18,13 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(8080, function (){
-	console.log('Express run on port 8080');
+
+
+
+
+
+
+
+http.listen(8080, function () {
+	console.log('Muy bien!, eres un crack, sabes trabajar con node JS');
 });
